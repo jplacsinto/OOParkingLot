66f2bdecf008ad377a99ea7ace61b1a1895ea975
JP Lacsinto <jplacsinto@gmail.com>
2023-09-12 15:00:03 +0800

    added parking id

--- app/Http/Resources/ParkingResource.php
+++ app/Http/Resources/ParkingResource.php
@@ -15,6 +15,7 @@ class ParkingResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'registration_id' => $this->registration_id,
             'date_parked' => $this->date_parked
         ];
